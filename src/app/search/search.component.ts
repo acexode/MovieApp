c2b8bce6fd7943d87ebf668be206c5f83f91a928
acexode <acecode009@gmail.com>
2017-05-15 00:55:57 +0300

    implemented search results

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MovieService } from '../service/movie.service'
-
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
@@ -10,12 +10,15 @@ export class SearchComponent implements OnInit {
   movie: String;
   Results: Array<Object>;
 
-  constructor(private service: MovieService) { }
+  constructor(private service: MovieService, private route: ActivatedRoute) { }
 
   ngOnInit() {
-   
-    this.service.search(this.movie).subscribe(res => this.Results = res.results);
+   this.route.params.subscribe((params) => {
+      const param = params['name']
+      this.service.search(param).subscribe(res => this.Results = res.results);
  
+   })
+    
   }
 
 }
